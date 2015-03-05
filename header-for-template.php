c5ec92007584b75552dc8530198fa373f5333e04
Ilse Alejo <ilse.alejo@gmail.com>
2015-03-04 23:43:43 -0600

    First Commit

---
+++ header-for-template.php
@@ -0,0 +1,15 @@
+<style type="text/css">
+#header { margin:10px auto 0 auto; min-width:960px; max-width:1200px; height:175px; background-image: url('images/tile-pale.jpg'); background-repeat: repeat; padding:0; color:white;
+}
+h1 {position:relative; top:40px; font-size:350%; color:white; margin:auto 0 auto 20px; width: 487px;
+}
+</style>
+<div id="header">
+<h1>This is the header</h1>
+<div id="reg-navigation">
+    <ul>
+        <li><a href="register-page.php">Register</a></li>
+        <li><a href="register-view_users-page.php">View Users</a></li>
+        <li><a href="register-password.php">New Password</a></li>
+</ul> </div>
+</div>
---
+++ template.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang=en>
+
+<head>
+	<title>T</title>
+	<meta charset=utf-8>
+	<link rel="stylesheet" type="text/css" href="includes.css">
+</head>
+<body>
+	<div id='container'>
+	<?php include('header-for-template.php'); ?> <?php include('nav.php'); ?>
+	<?php include('info-col.php'); ?>
+		<div id='content'><!--Start of page content.--> <h2>This is the Home Page</h2>
+			<p>The home page content. The home page content. The home page content. The home page content. The home page content. <br>The home page content. The home page content. The home page content. The home page content. <br>The home page content. The home page content. <br>The home page content. The home page content. The home page content. </p> <!--End of the home page content.-->
+		</div>
+	</div>
+	
+</html>
